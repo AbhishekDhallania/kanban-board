8a5cb4a2f1ca8389cd01355084518ced5728b8cd
AbhishekDhallania <dhallaniaabhishek@gmail.com>
2026-07-07 12:33:35 +0000

    project init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-400 h-screen">
+      <h1 className="p-4 text-center text-4xl">Kanban Board</h1>
+    </div>
+  );
+}
